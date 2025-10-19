8b621af40ee072f565d2f09bbd7abe81aaa31519
Ryan Taylor <rataylor93@gmail.com>
2025-10-19 13:30:56 -0400

    added HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        result = ""
+        for prop in self.props:
+            result += f' {prop}="{self.props[prop]}"'
+        return result
+
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
