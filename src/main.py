14de4aa1e1fa396f7ec486fa1fc1d64e93566d53
Ryan Taylor <rataylor93@gmail.com>
2025-10-19 22:28:01 -0400

    added content generation, tests, templates

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -10,9 +11,9 @@ template_path = "./template.html"
 
 
 def main():
-    # basepath = "/"
-    # if len(sys.argv) > 1:
-    # basepath = sys.argv[1]
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
@@ -20,6 +21,9 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 if __name__ == "__main__":
